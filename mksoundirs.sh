4487e503f74e862cdbe123b8d5ae0ee95836157a
TakaoTakahashi <tsq@gapj.net>
2022-05-30 12:16:43 +0900

    ディレクトリ作るのが面倒な人用

---
+++ mksoundirs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p /var/lib/asterisk/sounds/ja
+mkdir -p /var/lib/asterisk/sounds/ja/digits
+mkdir -p /var/lib/asterisk/sounds/ja/letters
+mkdir -p /var/lib/asterisk/sounds/ja/phonetic
+mkdir -p /var/lib/asterisk/sounds/ja/followme
