1423d6780512c5d3635c6178bd2220341ac5b4fe
TakaoTakahashi <tsq@gapj.net>
2022-06-01 09:22:10 +0900

    抜けていたものを修正

--- mksoundirs.sh
+++ mksoundirs.sh
@@ -5,3 +5,4 @@ mkdir -p /var/lib/asterisk/sounds/ja/digits
 mkdir -p /var/lib/asterisk/sounds/ja/letters
 mkdir -p /var/lib/asterisk/sounds/ja/phonetic
 mkdir -p /var/lib/asterisk/sounds/ja/followme
+mkdir -p /var/lib/asterisk/sounds/ja/local
