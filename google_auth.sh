05abd191ef3b717fbe8c22b0fa724058e6755dc6
root <tsq@gapj.net>
2024-02-19 03:52:11 +0900

    fix comment

--- google_auth.sh
+++ google_auth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export GOOGLE_APPLICATION_CREDENTIALS="/root/tts/silken-tenure-348401-691a4b3288c0.json"
+export GOOGLE_APPLICATION_CREDENTIALS="YOUR_CREDENTIAL_WILL_COME_HERE"
 gcloud auth application-default print-access-token
 
 exit 0
